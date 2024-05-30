8997cb86aaa153cabecd020da79b472ccd45f1f2
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-30 20:29:46 +0100

    develop home page

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,1 +1,29 @@
-<p> suppety sup </p>
+<main>
+  <img id="welcome" src="pycobytes-welcome.png">
+  
+  <p> Python is awesome. But much of its stacks of fascinating quirks, tricks, and other syntactic sugar good stuff tend to be hidden amidst ancient Stack Overflow posts and questionable reddit threads, which makes discovering it quite nontrivial. </p>
+  
+  <img id="glimpse" src="pycobytes-glimpse.png">
+
+  <p> So, <span class="pyb-flavour left">pyco</span><span class="pyb-flavour centre">:</span><span class="pyb-flavour right">bytes</span> is a weekly series where we delve into interesting and useful features in Python. This isn’t a comprehensive overview of the language by any means, but I share all the cool stuff I’ve discovered through years of adventuring. </p>
+</main>
+
+<style lang="scss">
+
+.content {
+  width: 100%;
+}
+
+img #welcome {
+  width: 100%;
+}
+
+span {
+  &.pyb-flavour {
+    &.left { color: $col-accent-prot }
+    &.centre { color: $col-grey-nova }
+    &.right { color: $col-accent-deut }
+  }
+}
+
+</style>
