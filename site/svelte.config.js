72ed75a244aaa1c2d7a24eb355066c83223cff47
Sup#2.0 <sup2point0@gmail.com>
2024-05-30 19:31:57 +0100

    fix SCSS preprocessing

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,16 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: "404.html",
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+    },
+  },
+  preprocess: [
+    sveltePreprocess({
+      scss: {
+        prependData: "@import './src/lib/styles/Nova.scss';",
+      }
+    }),
+    mdsvex({
+      extensions: [".md"]
+    })
+  ]
 };
 
 export default config;
