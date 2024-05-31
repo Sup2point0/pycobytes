71c247854b7a12974bab8a5bdbac5d30f2227e19
Sup#2.0 <sup2point0@gmail.com>
2024-05-31 09:45:24 +0100

    add `issue-index.py` for auto-collecting issues

---
+++ scripts/issue-index.py
@@ -0,0 +1,29 @@
+'''
+Index issues into `issues.config.js` for SvelteKit to use when building the site routes.
+'''
+
+from datetime import date
+from pathlib import Path
+
+
+ROOT = Path(__file__).parent.parent.absolute()
+ROUTE = ROOT / "site/src/lib/issues.config.js"
+
+
+files = (ROOT / "issues").glob("[0-9][0-9].md")
+issues = [str(int(file.stem)) for file in files]
+
+update = date.today().strftime("%b %d")
+
+content = f'''/// Issues Index
+/// last auto-generated: {update}
+
+export const ISSUES = [{",\n".join(issues)}];
+'''
+
+with open(ROUTE, "w") as source:
+  source.write(content)
+  # A little scuffed, but it works well
+
+
+print("--- DONE! ---")
---
+++ site/src/lib/issues.config.js
@@ -0,0 +1,4 @@
+/// Issues Index
+/// last auto-generated: May 31
+
+export const ISSUES = [1];
