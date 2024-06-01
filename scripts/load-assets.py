795543ad7d097308f4691824fad408e1a5712359
Sup#2.0 <sup2point0@gmail.com>
2024-06-01 21:14:35 +0100

    add `load-assets.py` for preparing asset files

---
+++ scripts/load-assets.py
@@ -0,0 +1,33 @@
+'''
+Copy project assets from `./assets` to `static/` for SvelteKit to use when building the site.
+'''
+
+import os
+import shutil
+from itertools import chain as ichain
+from pathlib import Path
+
+
+print(">>> Python / collecting assets...")
+
+
+ROOT = Path(__file__).parent.parent.absolute()
+
+
+## Load
+SOURCE = ROOT / "assets"
+
+assets = ichain(
+  SOURCE.glob("*.png"),
+  SOURCE.glob("*.jpg"),
+  SOURCE.glob("*.jpeg"),
+)
+
+
+## Save
+DEST = ROOT / "site/static"
+if not os.path.exists(DEST):
+  os.mkdir(DEST)
+
+for file in assets:
+  shutil.copyfile(file, DEST / file.name)
