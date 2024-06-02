964d6ca1a8c382555a5c9a262cfc7a8986fe7e48
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-02 19:28:07 +0100

    update footer styles

--- site/src/lib/parts/Footer.svelte
+++ site/src/lib/parts/Footer.svelte
@@ -4,35 +4,44 @@ let pageSourceLink = "https://github.com/Sup2point0/pycobytes";
 
 </script>
 
-<div class="footer">
-  <div class="footer-info">
-    <p> <span class="pyb-flavour left">pyco</span><span class="pyb-flavour centre">:</span><span class="pyb-flavour right">bytes</span>
-    <br> exploring the magic of Python, week by week </p>
-    <p> by Sup#2.0 </p>
-  </div>
-  <div class="footer-note">
-    <p> View on <a href={pageSourceLink}>GitHub</a> </p>
+<div class="footer-container">
+  <div class="footer">
+    <div class="info left">
+      <p> <span class="pyb-flavour left">pyco</span><span class="pyb-flavour centre">:</span><span class="pyb-flavour right">bytes</span>
+      <br> exploring the magic of Python, week by week </p>
+      <p> by <a href="https://github.com/Sup2point0">Sup#2.0</a> </p>
+    </div>
+    <div class="info right">
+      <p> Created with SvelteKit </p>
+      <p> Hosted on GitHub Pages </p>
+      <p> View on <a href={pageSourceLink}>GitHub</a> </p>
+    </div>
   </div>
 </div>
  
 <style lang="scss">
 
-.footer {
+.footer-container {
   width: 100%;
-  color: $grey-nova;
-  background-color: $grey-swallow;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: $grey-swallow;
 }
 
-.footer-info {
-  width: 100%;
+.footer {
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-items: space-between;
+  color: $grey-nova;
 }
 
-.footer-note {
-  width: 100%;
-  text-align: center;
+.left { text-align: left }
+.right { text-align: right }
+
+info {
+  min-width: 30vw;
 }
 
 span.pyb-flavour {
