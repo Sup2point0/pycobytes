ad15ccf2fdfbbb77887ce009572bd24b14623739
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-02 17:34:28 +0100

    stylise footer

--- site/src/lib/parts/Footer.svelte
+++ site/src/lib/parts/Footer.svelte
@@ -1,17 +1,17 @@
 <script>
 
-let pageSourceLink = "";
+let pageSourceLink = "https://github.com/Sup2point0/pycobytes";
 
 </script>
 
 <div class="footer">
   <div class="footer-info">
-    <p> pyco:bytes
-    <br> Exploring the magic of Python, week by week </p>
+    <p> <span class="pyb-flavour left">pyco</span><span class="pyb-flavour centre">:</span><span class="pyb-flavour right">bytes</span>
+    <br> exploring the magic of Python, week by week </p>
     <p> by Sup#2.0 </p>
   </div>
   <div class="footer-note">
-    <p> View source on <a href={pageSourceLink}>GitHub</a> </p>
+    <p> View on <a href={pageSourceLink}>GitHub</a> </p>
   </div>
 </div>
  
@@ -19,16 +19,26 @@ let pageSourceLink = "";
 
 .footer {
   width: 100%;
+  color: $grey-nova;
   background-color: $grey-swallow;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .footer-info {
-  padding-left: 20vw;
+  width: 100%;
 }
 
 .footer-note {
   width: 100%;
   text-align: center;
+}
+
+span.pyb-flavour {
+  &.left { color: $light-accent-prot; }
+  &.centre { color: $grey-nova; }
+  &.right { color: $light-accent-deut; }
 }
 
 </style>
