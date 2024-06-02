67bc6b1998a2155a1fc995c191e8e5ed6a862ea9
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-02 17:29:13 +0100

    update navbar parts and styles

--- site/src/lib/parts/Nav.svelte
+++ site/src/lib/parts/Nav.svelte
@@ -2,20 +2,41 @@
 
 import { base } from "$app/paths";
 
+import { ISSUES } from "$lib/issues.config.js";
+
 const navParts = [
   {
     align: "left", text: "pycobytes", pict: `${base}/pycobytes-title.png`,
     link: "https://sup2point0.github.io/pycobytes",
   },
-  {
-    align: "right", text: "Issues",
-    link: "https://sup2point0.github.io/pycobytes/issues",
-  },
+].concat([
   {
     align: "right", text: "About",
     link: "https://sup2point0.github.io/pycobytes/about",
-  }
-];
+  },
+  {
+    align: "right", text: "Issues",
+    dropdown: {
+      { text: "Index",
+        link: "https://sup2point0.github.io/pycobytes/issues" },
+      { text: "Latest",
+        link: `https://sup2point0.github.io/pycobytes/issues/${ISSUES[ISSUES.length -1]}` },
+      { text: "Random",
+        button: pickIssue },
+    },
+  },
+  {
+    align: "right", text: "Contact",
+    dropdown: {
+      { text: "Discuss",
+        link: "https://sup2point0.github.io/pycobytes/discussions" },
+      { text: "Submit an Idea",
+        link: "https://sup2point0.github.io/pycobytes/issues" },
+      { text: "Report a Bug",
+        link: "https://sup2point0.github.io/pycobytes/issues" },
+    },
+  },
+].reverse());
 
 </script>
 
@@ -23,13 +44,19 @@ const navParts = [
   <ul>
     {#each navParts as each}
       <li id={each.text} class={each.align}>
-        <a href={each.link}>
-          {#if each.pict}
-            <img src={each.pict} alt={each.text}>
-          {:else}
-            {each.text}
-          {/if}
-        </a>
+        {#if each.link}
+          <a href={each.link}>
+            {#if each.pict}
+              <img src={each.pict} alt={each.text}>
+            {:else}
+              {each.text}
+            {/if}
+          </a>
+        {:else if each.button}
+          <button on:click={each.button}> {each.text} </button>
+        {:else}
+          ?
+        {/if}
       </li>
     {/each}
   </ul>
@@ -40,14 +67,15 @@ const navParts = [
 nav {
   position: fixed;
   top: 0;
-  width: 100%;
+  max-width: 100%;
   height: 3rem;
   z-index: 2;
-  padding: 1rem;
-  display: flex;
+  padding: 0.5rem 20vw;
+  /* display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: center; */
   background-color: $blue-night;
+  background: linear-gradient(to bottom, $blue-night, color-mix(in srgb, $blue-night 90%, transparent));
 }
 .left { float: left; }
 .right { float: right; }
