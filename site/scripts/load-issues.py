5bcd1b433bcef78e027d4fe46ebbb374dd409f12
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-04 09:27:15 +0100

    update `load-issues.py` to extract metadata and inject frontmatter

--- site/scripts/load-issues.py
+++ site/scripts/load-issues.py
@@ -20,13 +20,55 @@ files = (ROOT / "issues").glob("[0-9][0-9].md")
 issues = []
 
 ROUTE = SRC / "exported"
+
 if not os.path.exists(ROUTE):
   os.mkdir(ROUTE)
 
 DEST = SRC / "exported"
+FIELDS = ["index", "title", "date", "topic"]
+
 for file in files:
   name = str(int(file.stem))
   issues.append(name)
+
+  ## PARSE
+  with open(file, "r") as source:
+    content = source.readlines()
+
+  if not any(("#PYCO live!" in line) for line in content[:4]):
+    continue
+
+  meta = False
+  live = False
+  fields = {}
+
+  for line in content[:5]:
+    if "-->" in line:
+      break
+
+    if not live:
+      if "#PYCO live!" in line:
+        live = True
+
+    for field in FIELDS:
+      if field in line:
+        *_, value = line.partition("= ")
+          fields[field] = value
+
+  if not live:
+    continue
+
+  front = f'''---
+    index: {fields["index"]}
+    title: {fields["title"]}
+    date: {fields.get("date", None)}
+    topic: {fields.get("topic", None)}
+---
+'''
+
+  with open(file, "w") as source:
+    source.seek(0)
+    source.write(front)
 
   shutil.copyfile(file, DEST / (name + ".svx"))
 
