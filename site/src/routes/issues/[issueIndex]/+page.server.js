1e7b21588cee99a48f76c588a4239e61a6b92feb
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-05 09:15:15 +0100

    try fix entries supplying

--- site/src/routes/issues/[issueIndex]/+page.server.js
+++ site/src/routes/issues/[issueIndex]/+page.server.js
@@ -3,6 +3,6 @@ import { ISSUES } from "$src/issues-config.js";
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
   return ISSUES.map(
-    issue => ({ issueIndex: toString(issue) })
+    issue => ({ issueIndex: toString(issue.issueIndex) })
   );
 }
