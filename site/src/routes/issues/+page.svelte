d86c5de1f3ef6dba36739de1c7a47c0ab5480958
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-05 09:23:22 +0100

    add back `issues/+page.svelte`

---
+++ site/src/routes/issues/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+
+import { ISSUES } from "$src/issues-config.js";
+
+import Header from "$src/parts/Header.svelte";
+import Article from "$src/parts/Article.svelte";
+
+</script>
+
+
+<svelte:head>
+  <title> Issues · pycobytes </title>
+</svelte:head>
+
+<Header title="Issues" />
+
+<Article>
+  <ul>
+    {#each ISSUES as issue}
+      <li>
+        <p> {issue.index} </p>
+        <a href={`issues/${issue.issueIndex}`}>{issue.title}</a>
+      </li>
+    {/each}
+  </ul>
+</Article>
+
+
+<style lang="scss">
+
+ul {
+  list-style-type: none;
+}
+
+</style>
+  
