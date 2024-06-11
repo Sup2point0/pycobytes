b046ee7814aa826bc705b74168f04d0290aec51a
Sup#2.0 <sup2point0@gmail.com>
2024-06-11 19:36:49 +0100

    configure issue#2

--- site/src/issues-config.js
+++ site/src/issues-config.js
@@ -1,10 +1,16 @@
 /// Issues Index
-/// last auto-generated: Jun 09
+/// last auto-generated: Jun 11
 
 const ISSUES = [{
   "name": "01",
   "issueIndex": "1",
   "title": "The Power of List Comprehensions",
   "date": "6 June 2024"
+},
+{
+  "name": "02",
+  "issueIndex": "2",
+  "title": "Stringterpolation with f-Strings",
+  "date": "12 June 2024"
 }];
 export default ISSUES;
