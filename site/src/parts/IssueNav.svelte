b5467472626ccb7eb10fa245d9f602047fe2fd54
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-06-24 11:46:31 +0100

    add `IssueNav.svelte`

---
+++ site/src/parts/IssueNav.svelte
@@ -0,0 +1,112 @@
+<script>
+
+import { base } from "$app/paths";
+
+import ISSUES from "#src/issues-config";
+
+export let issueData;
+
+
+const indexCurrent = issueData.orderIndex;
+
+// issue 0 (#1) has no previous
+const issuePrev =
+    (indexCurrent > 0)
+  ? ISSUES[indexCurrent -1]
+  : undefined;
+
+// latest issue has no next
+const issueNext =
+    (indexCurrent < ISSUES.length -1)
+  ? ISSUES[indexCurrent +1]
+  : undefined;
+
+</script>
+
+
+<div class="issue-nav">
+  {#if issuePrev !== undefined}
+    <a href="{base}/issues/{issuePrev.issueIndex}">
+      <button class="prev">
+        <div class="material-symbols-outlined"> left_arrow_ios </div>
+        <div>
+          <p> Previous </p>
+          <h4> {issuePrev.titleText} </h4>
+        </div>
+      </button>
+    </a>
+  {/if}
+
+  {#if issueNext !== undefined}
+    <a href="{base}/issues/{issueNext.issueIndex}">
+      <button class="next">
+        <div class="material-symbols-outlined"> right_arrow_ios </div>
+        <div>
+          <p> Next </p>
+          <h4> {nextPrev.titleText} </h4>
+        </div>
+      </button>
+    </a>
+  {/if}
+</div>
+
+
+<style lang="scss">
+
+.issue-nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+a {
+  min-width: 5rem;
+  color: normal;
+  text-decoration: none;
+}
+
+button {
+  padding: 0.5rem;
+  display: flex;
+  &.prev {
+    flex-direction: row;
+  }
+  &.next {
+    flex-direction: row-reverse;
+  }
+  justify-content: start;
+  align-items: center;
+  background-color: transparent;
+  border-radius: 1rem;
+  
+  transition: all 0.12s ease-out;
+  
+  &:hover {
+    background-color: $grey-swallow;
+  }
+
+  &:focus, &:active {
+    background-color: $grey-spirit;
+  }
+  
+  div {
+    padding: 0 0.5rem;
+    color: grey;
+  }
+
+  &.prev { text-align: left; ]
+  &.next { text-align: right; ]
+}
+ 
+p {
+  margin: 0;
+}
+
+h4 {
+  margin: 0;
+  padding: 0;
+  color: $pink-nova;
+}
+
+</style>
