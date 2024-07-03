358c9284e311e26c152ad036f155920fe3a6020e
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-03 12:02:36 +0100

    cleanup `IssueSearch`

--- site/src/parts/issue/IssueSearch.svelte
+++ site/src/parts/issue/IssueSearch.svelte
@@ -2,7 +2,7 @@
 
 import { ratio } from "fuzzball";
 
-import { searchData } from "#src/scripts/search";
+import { searchData } from "#src/scripts/stores/search";
 
 let value = "";
 
@@ -40,29 +40,29 @@ function searchIssues()
 search {
   width: 100%;
   padding: 1rem 0;
-	display: flex;
-	flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 input {
-	@include font-flavour;
-	padding: 1em;
+  @include font-flavour;
+  padding: 1em;
   flex-grow: 1;
-	border-radius: 0.5em;
+  border-radius: 0.5em;
 
-	&:focus {
-		outline: none;
-	}
+  &:focus {
+    outline: none;
+  }
 }
 
 button {
-	font-size: 100%;
-	color: white;
-	background-color: light-dark($col-accent, $col-flavour);
+  font-size: 100%;
+  color: white;
+  background-color: light-dark($col-accent, $col-flavour);
 
-	&:hover {
-		background-color: $pink-elec;
-	}
+  &:hover {
+    background-color: $pink-elec;
+  }
 }
 
 </style>
