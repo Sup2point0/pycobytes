41450525bfb73cf3b8c0c4adef644d84d972e938
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-03 11:55:58 +0100

    add `IssueSearch.svelte`

---
+++ site/src/parts/issue/IssueSearch.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+
+import { ratio } from "fuzzball";
+
+import { searchData } from "#src/scripts/search";
+
+let value = "";
+
+
+function searchIssues()
+{
+  $searchData.filterFunction = (issues) => (
+    issues.toSorted((prot, deut) =>
+      ratio(deut.titleText, value) - ratio(prot.titleText, value)
+    )
+  );
+}
+
+</script>
+
+
+<search>
+  <input
+    type="search"
+    placeholder="Search issues..."
+    maxlength="200"
+    bind:value
+  />
+
+  <button on:click={searchIssues}>
+    <span class="material-symbols-outlined">
+      search
+    </span>
+  </button>
+</search>
+
+
+<style lang="scss">
+
+search {
+  width: 100%;
+  padding: 1rem 0;
+	display: flex;
+	flex-direction: row;
+}
+
+input {
+	@include font-flavour;
+	padding: 1em;
+  flex-grow: 1;
+	border-radius: 0.5em;
+
+	&:focus {
+		outline: none;
+	}
+}
+
+button {
+	font-size: 100%;
+	color: white;
+	background-color: light-dark($col-accent, $col-flavour);
+
+	&:hover {
+		background-color: $pink-elec;
+	}
+}
+
+</style>
