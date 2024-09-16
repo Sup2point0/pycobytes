ad3d28f4d9b40dfe95027649711b4a6b63711f95
Sup#2.0 <sup2point0@gmail.com>
2024-09-16 20:59:40 +0100

    develop site structure

---
+++ site/src/scripts/utils/index.ts
@@ -0,0 +1,1 @@
+export { getIssue } from "./get-issue";
--- site/src/site.ts
+++ site/src/site.ts
@@ -1,10 +1,12 @@
+const data = await import("./data/site-data.json");
+
+
 const Site = {
   desc: {
     short: "exploring the magic of Python",
     long: "exploring the magic of Python, week by week",
-  }
+  },
+
+  issues: data.default.pages,
 }
 export default Site;
-
-const issues = await import("data/site-data.json");
-export const Issues = issues.default;
