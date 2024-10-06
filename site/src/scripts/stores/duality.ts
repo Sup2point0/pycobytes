ca34d851a37ef107b66037b26fdf429d5fc9d9dc
Sup#2.0 <sup2point0@gmail.com>
2024-10-06 14:06:46 +0100

    update duality handling

--- site/src/scripts/stores/duality.ts
+++ site/src/scripts/stores/duality.ts
@@ -1,5 +1,27 @@
 import { persisted } from "svelte-persisted-store";
 
+import { browser } from "$app/environment";
+
 
 type Duality = "light" | "dark" | null;
-export const duality = persisted<Duality>("pycobytes.duality", null);
+export const duality = persisted<Duality>("pycobytes.duality", getLocalDuality());
+
+
+function getLocalDuality(): string
+{
+  if (matchMedia) {
+    if (matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
+  }
+
+  return "light";
+}
+
+// Switch the duality. If none has been set, do nothing.
+function swapDuality()
+{
+  duality.update(d => d ? (d == "light" ? "dark" : "light") : null)
+}
