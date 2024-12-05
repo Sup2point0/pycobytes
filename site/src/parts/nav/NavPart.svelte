c05657f139470b53b64068f8b700387fbc02e7d8
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 18:30:15 +0000

    tweak styles

--- site/src/parts/nav/NavPart.svelte
+++ site/src/parts/nav/NavPart.svelte
@@ -96,7 +96,7 @@ ul.nav-dropdown {
   // opacity: 0;
   list-style-type: none;
   // transition: opacity 0.2s ease-out;
-  box-shadow: 0 4px 2px -1px rgb(black, 10%);
+  box-shadow: 0 4px 2px -2px rgb(black, 10%);
 }
 
 .nav-part:hover ~ ul.nav-dropdown,
